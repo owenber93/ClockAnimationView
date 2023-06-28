7eba3ecc395643f6711c601f5a486c116e930902
owenber93 <138006928+owenber93@users.noreply.github.com>
2023-06-28 06:38:39 -0400

    Add files via upload

---
+++ app/src/main/java/jp/shts/android/library/sample/clockanimationview/MainActivity.java
@@ -0,0 +1,46 @@
+package jp.shts.android.library.sample.clockanimationview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import jp.shts.android.library.clockanimationview.ClockAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ClockAnimationView clockAnimationView = (ClockAnimationView) findViewById(R.id.image);
+        clockAnimationView.setTime(0, 45);
+
+        final Button b = (Button) findViewById(R.id.button);
+        b.setText("to 2:30");
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (counter) {
+                    case 0:
+                        clockAnimationView.animateToTime(2, 30);
+                        b.setText("to 7:10");
+                        break;
+                    case 1:
+                        clockAnimationView.setTime(7, 10);
+                        b.setText("to 9:50");
+                        break;
+                    case 2:
+                        clockAnimationView.animateToTime(9, 50);
+                        b.setText("to Finish");
+                        break;
+                    default:
+                        finish();
+                        break;
+                }
+                counter++;
+            }
+        });
+    }
+    int counter=0;
+}
---
+++ app/src/main/java/jp/shts/android/library/sample/clockanimationview/MainApplication.java
@@ -0,0 +1,10 @@
+package jp.shts.android.library.sample.clockanimationview;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
